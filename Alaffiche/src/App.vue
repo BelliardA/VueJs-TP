56cbb6527b1ec3c61854b74f046091ff6b2647df
Arnaud <arnaud.belliard@crea-inseec.com>
2024-03-28 09:53:26 +0100

    add Cinemaheader fonctionnel

--- Alaffiche/src/App.vue
+++ Alaffiche/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
+import CinemaHeader from './components/icons/CinemaHeader.vue'
 </script>
 
 <template>
-  
-
-  
+  <CinemaHeader></CinemaHeader>
 </template>
 
 <style scoped>
+
 </style>
