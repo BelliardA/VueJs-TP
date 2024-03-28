7880d25bb02c8886b004234165ae94b3e5ed60f2
Arnaud <arnaud.belliard@crea-inseec.com>
2024-03-28 10:18:23 +0100

    add FilmCard

--- Alaffiche/src/App.vue
+++ Alaffiche/src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-import CinemaHeader from './components/icons/CinemaHeader.vue'
+import CinemaHeader from './components/CinemaHeader.vue'
+import FilmCard from './components/FilmCard.vue'
+import {films} from './assets/arena-lapraille.js'
 </script>
 
 <template>
   <CinemaHeader></CinemaHeader>
+
+  <div v-for="film in films">
+    <FilmCard :film="film"></FilmCard>
+  </div>
 </template>
 
 <style scoped>
---
+++ Alaffiche/src/components/FilmCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps(['film']);
+</script>
+
+<template>
+    <section class="Card">
+        <h2>titre : {{ film.titre }}</h2>
+        <h3 v-if="film.titreOriginal">titre original : {{ film.titreOriginal }}</h3>
+        <img :src="`${ film.imageURL }`" alt="">
+        <p>durée : {{ film.duree }} minutes</p>
+        <div class="mini-description">
+            <p>genre:</p>
+            <p v-for="unGenre in film.genre">{{ unGenre }}</p>
+        </div>
+        
+        
+    </section>
+</template>
+
+<style scoped>
+.Card{
+    display: flex;
+    flex-direction: column;
+    width: 370px;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding: 10px;
+    background-color: rgb(194, 194, 194);
+}
+.mini-description{
+    display: flex;
+    gap: 5px;
+}
+</style>
